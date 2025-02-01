011310b6963108295656c897b699cba432453713
Md Hasnath Karim <hasnath.karim@gmail.com>
2025-02-01 21:30:09 +0000

    added responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          'pulse-slow': 'pulse 10s infinite',
+
+        },
+      },
     },
     plugins: [],
   }
